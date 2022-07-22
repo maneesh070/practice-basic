be440c09f985465728a4c41a40d2afd9ffbc1e8a
Maneesh Kumar <maneeshkumar070@gmail.com>
2022-07-22 07:16:00 +0530

    Equal arrays basic method

---
+++ equal_arrays.py
@@ -0,0 +1,15 @@
+def check(A, B, n):
+    if len(A) != len(B):
+        return 0
+    i = 0
+    while i < n:
+        if A[i] in B:
+            i += 1
+        else:
+            return 0
+    return 1
+
+a = [1,2,3]
+b = [2,3,1,7]
+n = len(a)
+print(check(a,b,n))
